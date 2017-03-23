10a8dcb89b27b68386c4e7af8e16b87182e360c0
Kyle Jones <sornaensis@gmail.com>
2017-03-22 22:57:57 -0400

    Remote Compilation

--- tsl-compiler/Main.hs
+++ tsl-compiler/Main.hs
@@ -6,10 +6,8 @@ import qualified Data.ByteString.Lazy.Char8 as C
 
 import           Debug.Trace
 
-import           TSL.AST.Manipulation
 import           TSL.Compiler.Compiler
 import           TSL.Compiler.Types
-import           TSL.Parser.Parser
 
 main :: IO ()
 main =
@@ -19,13 +17,4 @@ main =
        (Just (TSLInput tls))   ->
             do theorems <- mapM genTheorem tls
                mapM_ (putStrLn . generateTheorem) theorems
-  where
-  genTheorem t@(TSLInputTheorem n code d i) =
-       do ts <- generateAllIneq 
-                    . concatMap replaceAllEqSign
-                    . (\(lets, ts) -> map (replaceAllInvar lets) ts) 
-                    . (\ts -> (extractLetStatements ts, ts)) 
-                    . theoremParser 
-                    . lexer $ trace code code
-          return $ TSLTheorem (TSLInputTheorem (n ++ show i) code d i) $! ts
 
--- tsl-compiler/TSL/Compiler/Compiler.hs
+++ tsl-compiler/TSL/Compiler/Compiler.hs
@@ -1,14 +1,17 @@
 module TSL.Compiler.Compiler (
         generateTheorem,
-        generateAllIneq
+        generateAllIneq,
+        genTheorem
         ) where
 
-import           Data.Set            (Set)
-import qualified Data.Set            as S
+import           Data.Set             (Set)
+import qualified Data.Set             as S
 
 import           TSL.AST.AST
+import           TSL.AST.Manipulation
 import           TSL.Compiler.Python
 import           TSL.Compiler.Types
+import           TSL.Parser.Parser
 
 generateTheorem :: TSLTheorem -> String
 generateTheorem = generatePythonClass
@@ -16,3 +19,13 @@ generateTheorem = generatePythonClass
 generateAllIneq :: [Fix Theorem] -> IO [Fix Theorem]
 generateAllIneq = fmap concat . mapM generateSymPyIneq
 
+genTheorem :: TSLInputTheorem -> IO TSLTheorem
+genTheorem (TSLInputTheorem n code d i) =
+     do ts <- generateAllIneq
+                  . concatMap replaceAllEqSign
+                  . (\(lets, ts) -> map (replaceAllInvar lets
+                                       . replaceAllInvar lets) ts)
+                  . (\ts -> (extractLetStatements ts, ts))
+                  . theoremParser
+                  . lexer $ code
+        return $ TSLTheorem (TSLInputTheorem (n ++ show i) code d i) $! ts
