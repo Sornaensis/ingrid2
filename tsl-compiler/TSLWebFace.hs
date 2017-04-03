5c6b56fe7695230ef9f0d18087dfddff1721a101
Kyle Jones <sornaensis@gmail.com>
2017-04-03 14:59:55 -0400

    Coalesce Web Interface

--- tsl-compiler/TSLWebFace.hs
+++ tsl-compiler/TSLWebFace.hs
@@ -88,7 +88,7 @@ postRPCIRR = do
     json <- requireJsonBody :: Handler Value
     case decode . cs $ json of
         Just (String tls) ->
-            do theorems <- liftIO $ E.handle ((\_ -> return []) :: SomeException -> IO [Fix Theorem]) (mapM (theorem . genTheorem) . theoremParser . lexer $ tls)
+            do theorems <- liftIO $ E.handle ((\_ -> return []) :: SomeException -> IO [Fix Theorem]) (mapM genTheorem tls)
                if null theorems
                   then returnJson $ object ["success" .= False, "output" .= ("Compiler error" :: Text)]
                   else do
