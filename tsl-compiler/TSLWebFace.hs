812fa73075df85b3d97508da62ed0adeed5671fc
Kyle Jones <sornaensis@gmail.com>
2017-04-03 15:12:36 -0400

    Coalesce Web Interface

--- tsl-compiler/TSLWebFace.hs
+++ tsl-compiler/TSLWebFace.hs
@@ -93,7 +93,7 @@ postRPCIRR = do
                if null theorems
                   then returnJson $ object ["success" .= False, "output" .= ("Compiler error" :: Text)]
                   else do
-                    let res = T.pack . unlines . map show . realizeAnalysis . theorem $ theorems
+                    let res = T.pack . unlines . (map show . realizeAnalysis) . theorem $ theorems
                     returnJson $ object ["success" .= True, "output" .= res]
         _  -> badResult json "Input Error"
 
