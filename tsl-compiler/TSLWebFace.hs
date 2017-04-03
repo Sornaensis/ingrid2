e5f6269dba9e0ecfbf682cd314737ef88eec26b1
Kyle Jones <sornaensis@gmail.com>
2017-04-03 13:01:48 -0400

    TSL IR Gen

--- tsl-compiler/TSLWebFace.hs
+++ tsl-compiler/TSLWebFace.hs
@@ -39,7 +39,7 @@ postCompileTSLR = do
                if null theorems
                   then badResult json "Compiler Error"
                   else do
-                    let res = T.pack . concatMap (map (show . realizeAnalysis) . theorem) $ theorems
+                    let res = T.pack . concatMap (concatMap (show . realizeAnalysis) . theorem) $ theorems
                     defaultLayout
                      [whamlet|
                        <div>
