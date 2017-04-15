b66b6b5747b91cad751c19d9e6b200d276c3daa0
Sornaensis <sornaensis@gmail.com>
2017-04-15 13:46:03 -0400

    Update theorems140.py

--- ingrid-mainline/theorems140.py
+++ ingrid-mainline/theorems140.py
@@ -1569,7 +1569,7 @@ class Theorem48(Theorem):
         return
 class Theorem49(Theorem):
     def __init__(self):
-        super(Theorem49, self).__init__(49, "if cycle then \n{\n    planar,\n    not forest,\n    crossing == 0.0,\n    nodes >= 3.0,\n    edges >= 3.0,\n    arboricity == 2.0,\n    nodeCover == floor((nodes+1.0)/2.0),\n    edgeCover == floor((nodes+1.0)/2.0),\n    nodeInd == floor(nodes/2.0),\n    edgeInd == floor(nodes/2.0),\n    radius == edgeInd,\n    girth == circumference,\n    circumference == nodes,\n    edgeChromatic == chromaticNum,\n    nodes >= 2.0*nodeCover-(1.0),\n    nodes <= 2.0*nodeCover,\n    nodes >= 2.0*edgeInd,\n    nodes <= 2.0*edgeInd+1.0,\n    nodeConnec == 2.0,\n    regular,\n    bandwidth == 2.0,\n    if nodes > 3.0 then \n    {\n        maxClique == 2.0\n    }\n    else if maxb(nodes) < 4.0 then \n    {\n        maxClique == 3.0\n    },\n    if even nodes then \n    {\n        chromaticNum == 2.0\n    }\n    else  \n    {\n        chromaticNum == 3.0\n    },\n    if chromaticNum == 2.0 then \n    {\n        even nodes\n    }\n    else  \n    {\n        odd nodes\n    },\n    if maxb(maxClique) == 2.0 then \n    {\n        nodes >= 4.0\n    }\n    else if minb(maxClique) == 3.0 then \n    {\n        nodes == 3.0\n    },\n    if nodes == 3.0 then \n    {\n        nodeCliqueCover == 1.0\n    }\n    else  \n    {\n        nodeCliqueCover == nodeCover\n    }\n};\n", "")
+        super(Theorem49, self).__init__(49, "if cycle then \n{\n    planar,\n    not forest,\n    crossing == 0.0,\n    nodes >= 3.0,\n    edges >= 3.0,\n    arboricity == 2.0,\n    nodeCover == floor((nodes+1.0)/2.0),\n    edgeCover == floor((nodes+1.0)/2.0),\n    nodeInd == floor(nodes/2.0),\n    edgeInd == floor(nodes/2.0),\n    radius == edgeInd,\n    girth == circumference,\n    circumference == nodes,\n    edgeChromatic == chromaticNum,\n    nodes >= 2.0*nodeCover-(1.0),\n    nodes <= 2.0*nodeCover,\n    nodes >= 2.0*edgeInd,\n    nodes <= 2.0*edgeInd+1.0,\n    nodeConnec == 2.0,\n    regular,\n    bandwidth == 2.0,\n    if nodes > 3.0 then \n    {\n        maxClique == 2.0\n    }\n    else if maxb(nodes) < 4.0 then \n    {\n        maxClique == 3.0\n    },\n    if even nodes then \n    {\n        chromaticNum == 2.0\n    }\n    else if odd nodes then \n    {\n        chromaticNum == 3.0\n    },\n    if maxb(chromaticNum) == 2.0 then \n    {\n        even nodes\n    }\n    else if minb(chromaticNum) == 3.0 then \n    {\n        odd nodes\n    },\n    if maxb(maxClique) == 2.0 then \n    {\n        nodes >= 4.0\n    }\n    else if minb(maxClique) == 3.0 then \n    {\n        nodes == 3.0\n    },\n    if nodes == 3.0 then \n    {\n        nodeCliqueCover == 1.0\n    }\n    else  \n    {\n        nodeCliqueCover == nodeCover\n    }\n};\n", "")
     def involves(self, str_invar):
         return str_invar in ["cycle","planar","forest","crossing","nodes","edges","arboricity","nodeCover","edgeCover","nodeInd","edgeInd","radius","girth","circumference","edgeChromatic","chromaticNum","nodeConnec","regular","bandwidth","maxClique","nodeCliqueCover"]
     def run(self):
@@ -1811,7 +1811,7 @@ class Theorem49(Theorem):
                     set("chromaticNum",  2.0, ind='Max')
                 except:
                     pass
-            elif True:
+            elif oddInvar("nodes"):
                 try:
                     set("chromaticNum",  3.0, ind='Min')
                 except:
@@ -1820,14 +1820,14 @@ class Theorem49(Theorem):
                     set("chromaticNum",  3.0, ind='Max')
                 except:
                     pass
-            if ((minb("chromaticNum") != 'undt' and minb("chromaticNum") >= 2.0) and (maxb("chromaticNum") != 'undt' and maxb("chromaticNum") <= 2.0)):
+            if ((maxb("chromaticNum") != 'undt' and maxb("chromaticNum") >= 2.0) and (maxb("chromaticNum") != 'undt' and maxb("chromaticNum") <= 2.0)):
                 if minb("nodes") != 'undt':
                     if even(minb("nodes")+1.0):
                         set("nodes", minb("nodes")+1.0, ind='Min')
                 if maxb("nodes") != 'undt':
                     if even(maxb("nodes")-(1.0)):
                         set("nodes", maxb("nodes")-(1.0), ind='Max')
-            elif True:
+            elif ((minb("chromaticNum") != 'undt' and minb("chromaticNum") >= 3.0) and (minb("chromaticNum") != 'undt' and minb("chromaticNum") <= 3.0)):
                 if minb("nodes") != 'undt':
                     if odd(minb("nodes")+1.0):
                         set("nodes", minb("nodes")+1.0, ind='Min')
